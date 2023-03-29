19b2e36180ae9b9a2e38249c6213879a266f10ab
Mesrop <105871863+ArtMonicc@users.noreply.github.com>
2023-03-29 21:08:10 +0300

    ДЗ Меню 2

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,20 +1,14 @@
-const subMenu = Array.from(document.querySelectorAll('.menu_sub'));
-const links = Array.from(document.querySelectorAll('.menu__link'));
+const links = Array.from(document.querySelectorAll(".menu__link"));
+const subMenu = Array.from(document.querySelectorAll(".menu_sub"));
 
-for (let i = 0; i < links.length; i++) {
-    if (links[i].nextElementSibling != null) {
-        links[i].onclick = function () {
-            if (links[i].nextElementSibling.classList.contains('menu_active')) {
-                links[i].nextElementSibling.classList.remove('menu_active');
-            } else {
-                closeSubMenu()
-                links[i].nextElementSibling.classList.add('menu_active');
-            }
-            return false;
-        }
+links.forEach((link) => {
+  link.onclick = () => {
+    if (link.parentElement.querySelector(".menu_sub") !== null) {
+      subMenu.forEach((item) => item.classList.remove("menu_active"));
+      link.parentElement
+        .querySelector(".menu_sub")
+        .classList.add("menu_active");
+      return false;
     }
-}
-
-function closeSubMenu() {
-    subMenu.forEach(elem => elem.classList.remove('menu_active'));
-}
+  };
+});
