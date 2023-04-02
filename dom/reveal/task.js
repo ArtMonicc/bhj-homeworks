02af1dbf7d3aa0f96702f0f820df57ecf621fb55
Mesrop <105871863+ArtMonicc@users.noreply.github.com>
2023-04-02 13:51:11 +0300

    ДЗ Прокрутка и элементы 1

---
+++ dom/reveal/task.js
@@ -0,0 +1,15 @@
+const revealEl = Array.from(document.querySelectorAll(".reveal"));
+
+document.addEventListener("scroll", isVisible);
+
+function isVisible() {
+  revealEl.forEach((elem) => {
+    const { innerHeight } = window;
+    const { top } = elem.getBoundingClientRect();
+    if (top < innerHeight && top > 0) {
+      elem.classList.add("reveal_active");
+    } else {
+      elem.classList.remove("reveal_active");
+    }
+  });
+}
